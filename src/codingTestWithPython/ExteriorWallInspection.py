54b1363b9b7e741f58f502d3d0bad5ffbeaf5c8c
ychic <officialyoonsic@gmail.com>
2022-01-02 01:38:22 +0900

    #add 외벽 문제
    6 10 12 14번 틀린 케이스
    -원인 예측 dist permutation 없이 불가능한 케이스
    only greedy로는 구현에 한계가 있음을 반증하는 예제 추가

--- src/codingTestWithPython/ExteriorWallInspection.py
+++ src/codingTestWithPython/ExteriorWallInspection.py
@@ -1,58 +1,89 @@
 import copy
 import itertools
 
+
 def solution(n, weak, dist):
-    answer = -1
+    answer = 0
+
+    circle_set = []
+    tmp = list(weak)
+    for _ in range(len(weak)):
+        tmp.append(tmp.pop(0))
+        circle_set.append(copy.deepcopy(tmp))
+
+    # print(circle_set)
     dist.sort(reverse=True)
-    print("점검의 모든 경우의 수")
-    orders_weak = list(itertools.permutations(weak,len(weak)))
-    print(orders_weak)
-    print(len(orders_weak))
-    print("모든 dist의 수")
-    orders_dist = list(itertools.permutations(dist,len(dist)))
-    print(len(orders_dist))
-    print(orders_dist)
 
-    #dist 인원을 기준으로 구해보자
-    res =9999
-    #특정 dist set을 기준으로
-    # for kind_dist in orders_dist:
-        #테스팅할 weak order를 가져옴
-    for kind_weak in orders_weak:
-        tmp = list(copy.deepcopy(kind_weak))
-        #해당 dist set을 하나씩 더하며 갈 수 있는지 확인
-        for i in range(len(dist)):
-            standard = tmp.pop(0)
-            if not tmp:
-                res =min(res,i+1)
-                break
-            else:
-                # start = standard-kind_dist[i] if standard-kind_dist[i] <0 else standard-kind_dist[i] + 12
-                # end = standard+kind_dist[i] if standard+kind_dist[i] <12 else standard+kind_dist[i] - 12
-                ran =[standard]
-                for k in range(1,dist[i]+1):
-                    ran.append(ran[0]+k if ran[0]+k < 12 else ran[0]+k-12)
+    #결과를 담기위한 변수
+    res=[]
 
-                # print(standard,'-',kind_dist[i],': ',ran)
-                toggle =False
-                while True:
-                    if tmp and tmp[0] in ran:
-                        tmp.pop(0)
-                        toggle = True
-                    else:
-                        if not toggle:
-                            #다시 삽입
-                            tmp.insert(0,standard)
-                        break
+    #시작점만 돌리고 순서는 변동하지 않음
+    for kind_weak in circle_set:
+        print("현재 외벽 케이스 :" , kind_weak)
+        #각 kind_weak에서 점검이 완료된 외벽의 갯수
+        clear = 0;
+        #점검을 위한 인원
+        need = 0
+        #현재 작업하려는 외벽의 위치
+        cur_idx = 0
+        while True:
+            #인원을 먼저  가용
+            need +=1
 
-                print("남은 것들",tmp)
-                if not tmp :
-                    res =min(res,i+1)
-                    break
+            #외벽 수 만큼 점검이 완료 됨 => 점검 완료 case
+            if clear == len(kind_weak):
+                res.append(need-1)
+                break;
 
-        print("weak set 결과 :" ,res)
+            #필요인원이 가용인원보다 많다 => 불가능 case
+            if need > len(dist):
+                break;
 
-    return res
+            #해당 인원이 최소 한개의 점검을 담당하므로 +1
+            clear+=1
+            #해당 인원이 현재 외벽에서 점검 할 수 있는 위치를 담는 리스트
+            fix_range=[]
+            for i in range(1,dist[need-1]+1):
+                movable = kind_weak[cur_idx]+i
+                fix_range.append(movable if movable < n else movable-n)
+
+            print("현재 외벽 위치가 {} 번 인덱스 일 때 {} 거리를 담당 할 수 있는 사람이 움직 일 수 있는 범위 : {}".format(cur_idx,dist[need-1],fix_range))
+            # 해당 인원 역량검사
+            print('<-- 역량 체크 -->')
+            for check in range(cur_idx+1,len(kind_weak)):
+                #해당 인원이 점검할 수 있는 범위안에 있다면 clear 횟수를 증가
+
+                if kind_weak[check] in fix_range:
+                    print('{}번째 인덱스인 {} 점검 가능 추가 '.format(check,kind_weak[check]))
+                    clear+=1
+                    #현재 작업 위치를 옮김
+                    cur_idx =check
+            print('<-- 역량 체크 -->')
+            # 여기까지 돌았다면, 인원하나를 뽑아 최소 하나 이상의 외벽을 점검했고, for문으로 인해 갈 수 있는 곳의 외벽까진 점검한 상태임
+            # 이제 다음 외벽을 준비해야됨
+            cur_idx +=1
+            print("다음에 점검할 타겟 외벽 위치 : " ,cur_idx)
+            print("현재까지 사용한 인원 : ",need)
+            print("점검완료 갯수 : ",clear)
+        print("[--- 한세트 끝 --]")
+
+    # print(res)
+    return min(res) if res else -1
+
+
+def getPossibleIdx(kind_weak, idx, n, dist):
+    for i in range(idx + 1, len(kind_weak)):
+        prev = list(kind_weak)[i - 1]
+        now = list(kind_weak)[i]
+        if (prev > now):
+            now += n
+        if prev + dist > now:
+            idx += 1
+        else:
+            break;
+    # print('idx 값 : ',idx)
+    return idx
+
 
 #
 # #원형에서 첫번째 기준으로 왼쪽으로 second까지 순차거리
@@ -78,8 +109,14 @@ def solution(n, weak, dist):
 #
 #
 #
-# print(solution(12,[1, 5, 6, 10],[1, 2, 3, 4]))
-print(solution(12,[1, 3, 4, 9, 10],[3, 5, 7]))
-
-# list_=[1]
-# print(type([1]),list_.pop(0),list_)
+# print(solution(12,[1, 5, 6, 10],[1, 2, 3, 4])) #2
+# print(solution(12,[1, 3, 4, 9, 10],[3, 5, 7])) #1
+# print(solution(200,[0,100],[1,1])) # 2
+# print(solution(200,[50,100],[1])) # -1
+# print(solution(200,[50],[1])) #1
+# print(solution(50,[1,25],[6])) #-1`
+# print(solution(50,[1,25],[6])) #-1`
+# print(solution(200,[0,100],[1,1])) #2
+# print(solution(200,[1,50,151,155],[50,1])) #2
+# print(solution(12,[10,0],[1,2])) #1
+print(solution(200,[0,10,50,80,120,160],[1,10,5,40,30]))
