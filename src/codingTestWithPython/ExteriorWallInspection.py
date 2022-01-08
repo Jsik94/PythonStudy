c6c4978227960c722ebadf0a73429d3cb17139b2
ychic <officialyoonsic@gmail.com>
2022-01-08 14:06:40 +0900

    #add 메뉴리뉴얼 13,14,15 시간초과 이슈

--- src/codingTestWithPython/ExteriorWallInspection.py
+++ src/codingTestWithPython/ExteriorWallInspection.py
@@ -1,4 +1,3 @@
-import copy
 import itertools
 
 
@@ -9,23 +8,19 @@ def solution(n, weak, dist):
     tmp = list(weak)
     for _ in range(len(weak)):
         tmp.append(tmp.pop(0))
-        circle_set.append(copy.deepcopy(tmp))
+        circle_set.append(tmp.copy())
 
     # print(circle_set)
     dist.sort(reverse=True)
-    # circle_set =[]
-    # circle_set.append(list(weak))
-    # print()
     # 결과를 담기위한 변수
     res = []
 
     dists = list(itertools.permutations(dist,len(dist)))
-    # print(dists)
-    # 시작점만 돌리고 순서는 변동하지 않음
-    for kind_weak in circle_set:
-        # print("현재 외벽 케이스 :", kind_weak)
 
-        for kind_dist in dists:
+    for kind_dist in dists:
+
+
+        for kind_weak in circle_set:
             # print("-> 현재 dist 케이스 ", kind_dist)
             # kind_dist=(10, 30, 40, 5, 1)
             # 각 kind_weak에서 점검이 완료된 외벽의 갯수
@@ -34,6 +29,10 @@ def solution(n, weak, dist):
             need = 0
             # 현재 작업하려는 외벽의 위치
             cur_idx = 0
+            #
+            # if kind_weak == [9,10,1,3,4] :
+            #     print('여기~')
+
             while True:
                 # 인원을 먼저  가용
                 need += 1
@@ -44,7 +43,6 @@ def solution(n, weak, dist):
                 # 필요인원이 가용인원보다 많다 => 불가능 case
                 if need > len(kind_dist):
                     break;
-
                 # 해당 인원이 최소 한개의 점검을 담당하므로 +1
                 clear += 1
                 # 해당 인원이 현재 외벽에서 점검 할 수 있는 위치를 담는 리스트
@@ -52,13 +50,11 @@ def solution(n, weak, dist):
                 for i in range(1, kind_dist[need - 1] + 1):
                     movable = kind_weak[cur_idx] + i
                     fix_range.append(movable if movable < n else movable - n)
-
                 # print("현재 외벽 위치가 {} 번 인덱스 일 때 {} 거리를 담당 할 수 있는 사람이 움직 일 수 있는 범위 : {}".format(cur_idx, dist[need - 1],fix_range))
                 # # 해당 인원 역량검사
                 # print('<-- 역량 체크 -->')
                 for check in range(cur_idx + 1, len(kind_weak)):
                     # 해당 인원이 점검할 수 있는 범위안에 있다면 clear 횟수를 증가
-
                     if kind_weak[check] in fix_range:
                         # print('{}번째 인덱스인 {} 점검 가능 추가 '.format(check, kind_weak[check]))
                         clear += 1
@@ -71,11 +67,9 @@ def solution(n, weak, dist):
                 # print("다음에 점검할 타겟 외벽 위치 : ", cur_idx)
                 # print("현재까지 사용한 인원 : ", need)
                 # print("점검완료 갯수 : ", clear)
-            # print("[--- 한세트 끝 --]")
-
-
-
+        # print("[--- 한세트 끝 --]")
     # print(res)
+
     return min(res) if res else -1
 
 
@@ -93,7 +87,7 @@ def getPossibleIdx(kind_weak, idx, n, dist):
     return idx
 
 
-print(solution(12,[1, 5, 6, 10],[1, 2, 3, 4])) #2
+# print(solution(12,[1, 5, 6, 10],[1, 2, 3, 4])) #2
 print(solution(12,[1, 3, 4, 9, 10],[3, 5, 7])) #1
 print(solution(200,[0,100],[1,1])) # 2
 print(solution(200,[50,100],[1])) # -1
---
+++ src/codingTestWithPython/menuRenual.py
@@ -0,0 +1,60 @@
+import itertools
+def solution(orders, course):
+    answer = []
+    cnt_combi =[0 for _ in range(len(orders))]
+    total =set()
+    for i in range(len(orders)):
+        # print(set(list(orders[i])))
+        tmp =set(list(orders[i]))
+        cnt_combi[i] = set(list(orders[i]))
+        total = total| tmp
+
+    # print('집합분리',cnt_combi)
+    total = list(total)
+    total.sort()
+    #     print(list(itertools.combinations(total,2)))
+
+    #     print(''.join(list(total)))
+
+    # print('총 문자합',total)
+    for cnt in course:
+        targets=[]
+        for target in list(itertools.combinations(set(total),cnt)):
+            targets.append(target)
+
+        # print(targets)
+        # print('fun--------------------------------')
+        results = getMaxMenu(cnt_combi,targets)
+        for result in results:
+            answer.append(result)
+
+    answer.sort()
+    return answer
+
+#origin을 target으로 검사 -> 가장 큰값을 반환 그러나 큰값이 여러개면 같이 반환
+def getMaxMenu(origins,targets):
+    result =dict()
+    for target in targets:
+        target = set(target)
+        cnt = 0
+        # print('target으로 비교 시작',target)
+        for origin in origins:
+            # print('->대상 :',origin)
+            if target == origin.intersection(target):
+                cnt+=1
+
+        if cnt > 1 :
+            target = list(target)
+            target.sort()
+            result[''.join(target)]= cnt
+
+    # print('결과',result)
+    # key = max(result,key=result.get)
+    # val = max(result.values())
+    # print('큰값:',val)
+    output = [keys for keys,v in result.items() if max(result.values())== v  ]
+    # print('아웃:',output)
+    return output
+# print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4]))
+print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"],[2,3,5]))
+# print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4]))
